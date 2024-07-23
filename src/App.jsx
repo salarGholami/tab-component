545b8d6f013b23bbdfae486c7991a67c8e285318
salarGholami <salargholami021@gmail.com>
2024-07-23 21:31:48 +0330

    Tab Component Markup

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+
+const tabData = [
+  {
+    id: 1,
+    title: "ITEM 1",
+    content: "Authorize the user data ✅",
+  },
+  {
+    id: 2,
+    title: "ITEM 2",
+    content: "Redirect user to cart page 🛒",
+  },
+  {
+    id: 3,
+    title: "ITEM 3",
+    content: "Create new payment for the user 💰",
+  },
+];
+
+function App() {
+  const activeTab = 3;
+  return (
+    <div className="tab">
+      <div className="tab__header">
+        {tabData.map((tab) => (
+          <button key={tab.id} className={activeTab === tab.id ? "active" : ""}>
+            <span>{tab.title}</span>
+            <span className="tab-indicator"></span>
+          </button>
+        ))}
+      </div>
+      <div className="tab__content">{tabData[activeTab - 1].content}</div>
+    </div>
+  );
+}
+
+export default App;
