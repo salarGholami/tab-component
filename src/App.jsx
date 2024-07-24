e7f3909fd35034712c27ecb64c87910ebb0a2015
salarGholami <salargholami021@gmail.com>
2024-07-24 16:49:13 +0330

    Adding another state

--- src/App.jsx
+++ src/App.jsx
@@ -20,27 +20,41 @@ const tabData = [
 ];
 
 function App() {
-  const [activeTab,setActiveTab] = useState(1);
+  const [activeTab, setActiveTab] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handleActiveTab = (id) => {
-    setActiveTab(id)
+    setActiveTab(id);
+  };
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
-    <div className="tab">
-      <div className="tab__header">
-        {tabData.map((tab) => (
-          <button
-            onClick={() => handleActiveTab(tab.id)}
-            key={tab.id}
-            className={activeTab === tab.id ? "active" : ""}
-          >
-            <span>{tab.title}</span>
-            <span className="tab-indicator"></span>
-          </button>
-        ))}
-      </div>
-      <div className="tab__content">{tabData[activeTab - 1].content}</div>
+    <div>
+      <button onClick={() => handleOpen()}>❌</button>
+      {isOpen ? (
+        <div className="tab">
+          <div className="tab__header">
+            {tabData.map((tab) => (
+              <button
+                onClick={() => handleActiveTab(tab.id)}
+                key={tab.id}
+                className={activeTab === tab.id ? "active" : ""}
+              >
+                <span>{tab.title}</span>
+                <span className="tab-indicator"></span>
+              </button>
+            ))}
+          </div>
+          <div className="tab__content">{tabData[activeTab - 1].content}</div>
+        </div>
+      ) : (
+        <div>
+          <p>closed</p>
+        </div>
+      )}
     </div>
   );
 }
